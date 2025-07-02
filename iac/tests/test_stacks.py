8fda87888aa54e8a2cb970d5d4edbd6701e8d969
Brian Fogarty <brian@fogarty.email>
2025-07-01 21:44:55 -0400

    chore: improve output of valid terraform test

--- iac/tests/test_stacks.py
+++ iac/tests/test_stacks.py
@@ -6,4 +6,5 @@ from lab.stacks import Lab
 class TestLab:
     def test_terraform_is_valid(self):
         lab_stack = Lab(cdktf.Testing.app(), "lab")
-        assert cdktf.Testing.to_be_valid_terraform(cdktf.Testing.full_synth(lab_stack))
+        dst = cdktf.Testing.full_synth(lab_stack)
+        assert cdktf.Testing.to_be_valid_terraform(dst), f"terraform is not valid: {dst}"
