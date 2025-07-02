6f1347819d87c8a65931533528c5e7f10857af04
Brian Fogarty <brian@fogarty.email>
2025-07-01 21:58:16 -0400

    fix: format

--- iac/tests/test_stacks.py
+++ iac/tests/test_stacks.py
@@ -12,4 +12,6 @@ class TestLab:
     def test_terraform_is_valid(self):
         lab_stack = Lab(cdktf.Testing.app(), "lab")
         dst = cdktf.Testing.full_synth(lab_stack)
-        assert cdktf.Testing.to_be_valid_terraform(dst), f"terraform is not valid: {dst}"
+        assert cdktf.Testing.to_be_valid_terraform(
+            dst
+        ), f"terraform is not valid: {dst}"
